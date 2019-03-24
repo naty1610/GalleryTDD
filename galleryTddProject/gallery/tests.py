79a9e2c45c8172503a5c669b1214cf470fcce4da
vivasoft <vargaspintoadriana@gmail.com>
2019-03-24 09:31:52 -0500

    Paso 3: Adicionar un usuario (ROJO)

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -24,3 +24,24 @@ class GalleryTestCase(TestCase):
         current_data=json.loads(response.content)
 
         self.assertEqual(len(current_data), 2)
+
+    def test_verify_first_from_images_list(self):
+        user_model = Usuario.objects.create(username="testUser", name="Test",
+                                            lastname="User", password="AnyPas#5", email="test@test.com",
+                                            photo="https://banner2.kisspng.com/20180331/czw/kisspng-computer-icons-user-profile-female-avatar-user-5abff416099122.7881303215225293020392.jpg",
+                                            educationLevel="Pregrado", profession="Ingeniera de sistemas")
+
+        Image.objects.create(name='nuevo1', url='No', description='testImage', type='jpg', user=user_model)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model)
+
+        response = self.client.get('/gallery/')
+        current_data = json.loads(response.content)
+
+        self.assertEqual(current_data[0]['fields']['name'], "nuevo1")
+
+    def test_add_user(self):
+        response = self.client.post('/gallery/addUser/', json.dumps(
+            {"username": "testUser", "name": "Test", "lastname": "User", "password": "AnyPas#5",
+             "email": "test@test.com", "photo": "https://banner2.kisspng.com/20180331/czw/kisspng-computer-icons-user-profile-female-avatar-user-5abff416099122.7881303215225293020392.jpg", "educationLevel":"Pregrado", "profession":"Ingeniera de sistemas"}), content_type='application/json')
+        current_data = json.loads(response.content)
+        self.assertEqual(current_data[0]['fields']['username'], 'testUser')
